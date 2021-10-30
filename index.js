ff2b93bfe9382759b2c55205000b34ccab8318ac
israt-emu <isratjahan.emu1998@gmail.com>
2021-10-30 10:33:03 +0600

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4g4am.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//function to connect database
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("products2");
+    const productsCollection = database.collection("productsCollection");
+    console.log("connected");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
